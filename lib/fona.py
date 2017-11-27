cd9df3ad9cff58002996a8b8e3796f9bfddb6430
John Marzulli <john.marzulli@hotmail.com>
2017-11-27 04:26:29 +0000

    Finally got "send_message" working!!!

--- lib/fona.py
+++ lib/fona.py
@@ -2,6 +2,49 @@
 import time
 
 SECONDS_TO_WAIT_AFTER_SEND = 5
+BATTERY_CRITICAL = 40
+BATTERY_WARNING = 60
+DEFAULT_RESPONSE_READ_TIMEOUT = 5
+
+
+class Battery_Condition:
+    """
+    Class to keep the battery state.
+    """
+
+    def is_battery_ok(self):
+        """
+        Is the battery OK?
+        """
+        return self.battery_percent
+
+    def __init__(self, command_result):
+        """
+        Initialize.
+        """
+        self.error_state = False
+
+        if command_result is None or len(command_result) < 1:
+            self.error_state = True
+        else:
+            tokens = command_result.split(':')
+        
+            if tokens is None or len(tokens) <=1:
+                self.error_state = True
+
+            results = tokens[1].split(',')
+
+            if results is None or len(results) <= 2:
+                self.error_state = True
+
+        if not self.error_state:
+            self.charge_state = float(results[0])
+            self.battery_percent =float(results[1])
+            self.milliamp_hours = float(results[2]) / 10.0
+        else:
+            self.charge_state = 0
+            self.battery_percent = 0
+            self.milliamp_hours = 0
 
 
 class Sms_Message(object):
@@ -38,12 +81,103 @@ class Fona(object):
 
     def __init__(self, name, ser, allowednumbers):
         self.name = name
+        self.command_history = []
         self.serial_connection = ser
+        self.serial_connection.flushInput()
+        self.serial_connection.flushOutput()
         self.allowednumbers = allowednumbers
+        self.send_command("AT")
+        #self.send_command("AE0")
+        self.disable_verbose_errors()
+        self.set_sms_mode()
 
-    def send_command(self, com):
+        print self.read_from_fona(10)
+
+    def write_to_fona(self,
+                      text):
+        """
+        Write text to the Fona in a safe manner.
+        """
+        # print "Checking connection"
+        if self.serial_connection is None:
+            return "NO CON"
+
+        # print "Writting to serial"
+        num_bytes_written = self.serial_connection.write(text)
+        self.serial_connection.flush()
+
+        self.command_history.append("self.write_to_fona(" + self.escape(text) + ")")
+
+        # print "Checking return"
+        # print "Wrote " + str(num_bytes_written) + ", expected " + str(len(text))
+
+        self.wait_until_fona_command_response()
+
+        return num_bytes_written
+
+    def read_from_fona(self, response_timeout=2):
+        """
+        Read back from the Fona in a safe manner.
+        """
+        read_buffer = ""
+        start_time = time.time()
+
+        self.command_history.append("self.read_from_fona(" + str(response_timeout) + ")")
+
+        # print "   starting read"
+        while self.serial_connection.inWaiting() > 0:
+            # print "******"
+            read_buffer += self.serial_connection.read(1)
+            time_elapsed = time.time() - start_time
+            if time_elapsed > response_timeout:
+                print "TIMEOUT"
+                break
+
+        # print "   done"
+        return read_buffer
+
+    def simple_terminal(self):
+        """
+        Simple interactive terminal to play with the Fona.
+        """
+
+        should_quit = False
+        ret = []
+        start_time = time.time()
+
+
+        while not should_quit:
+            try:
+                command = str(input("READY:"))
+                if command == "quit":
+                    should_quit = True
+                else:
+                    ret.append("time.sleep(" + str(time.time() - start_time) + ")")
+                    write_result = self.write_to_fona(command)
+                    start_time = time.time()
+                    print write_result
+
+                    print self.read_from_fona(DEFAULT_RESPONSE_READ_TIMEOUT)
+            except error:
+                print "ERROR:" + str(error)
+            
+        for command in self.command_history:
+            print command
+
+    def escape(self, text):
+        """
+        Replaces escape sequences do they can be printed.
+        """
+        return text.replace('\r', '\\r').replace('\n', '\\n').replace('\x1a','\\x1a')
+
+
+    def send_command(self, com, add_eol=True):
         """ send a command to the modem """
-        self.serial_connection.write(com + "\r\r\n ")
+        command = com
+        if add_eol:
+            command += '\r\r\n '
+        self.command_history.append("self.send_command(" + self.escape(command) + ")")
+        self.serial_connection.write(command)
         time.sleep(2)
         ret = []
 
@@ -57,6 +191,9 @@ class Fona(object):
                 ret.append(msg)
         return ret
 
+    def disable_verbose_errors(self):
+        return self.send_command("AT+CMEE=0")
+
     def enable_verbose_errors(self):
         return self.send_command("AT+CMEE=2")
 
@@ -66,8 +203,14 @@ class Fona(object):
     def get_signal_strength(self):
         return self.send_command("AT+CSQ")
 
-    def get_battery_state(self):
-        return self.send_command("AT+CBC")
+    def get_current_battery_condition(self):
+        command_result = self.send_command("AT+CBC")
+        if command_result is None or len(command_result) < 2:
+            command_result = None
+        else:
+            command_result = command_result[1]
+
+        return Battery_Condition(command_result)
 
     def get_module_name(self):
         return self.send_command("ATI")
@@ -86,30 +229,76 @@ class Fona(object):
         self.set_sms_mode()
         return self.send_command('AT+CMGL="ALL"')
 
+    def clean_phone_number(self, phone_number):
+        """
+        Removes any text from the phone number that
+        could cause the command to not work.
+        """
+        if phone_number:
+            return phone_number.replace('+', '').replace('(', '').replace(')',
+                '').replace('-', '')
+
+        return None
+
+    def read_until_text(self, text):
+        """
+        Reads from the fona until the text is found.
+        """
+
+        read_text = ""
+        start_time = time.time()
+
+        while text not in read_text:
+            self.wait_until_fona_command_response()
+            read_text += self.read_from_fona(2)
+            elapsed_time = time.time() - start_time
+
+            if elapsed_time > 10:
+                print "TIMEOUT"
+                break
+
+        return read_text
+
+    def wait_until_fona_command_response(self):
+        """
+        Waits until the command has a response
+        """
+        self.command_history.append("self.wait_until_fona_command_response()")
+        start_time = time.time()
+        while time.time() - start_time < 2 and self.serial_connection.inWaiting() < 1:
+            time.sleep(0.5)
+
+        return self.serial_connection.inWaiting() > 0
+
     def send_message(self, message_num, text):
-        """ send a message to the specified phone number """
+        """
+        Sends a message to the specified phone numbers.
+        """
 
-        print "Starting send_message(" + message_num + ", " + text + ")"
+        cleaned_number = self.clean_phone_number(message_num)
 
-        if message_num is None or text is None:
+        if cleaned_number is None or text is None:
             return
 
-        if not message_num.startswith('"+'):
-            print "Adding AT command to number"
-            message_num = '"+' + message_num + '"'
-
-        self.set_sms_mode()
         print "Setting receiving number..."
-        print self.send_command('AT+CMGS='
-                                + message_num
-                                + '\r\r\n'
-                                + text
-                                + "\x1a")
-        # print "Sending text body"
-        # print self.send_command(text + chr(26))
-        print "sent."
-        time.sleep(SECONDS_TO_WAIT_AFTER_SEND)
+        self.set_sms_mode()
+        self.write_to_fona('\r\r\n')
+        if self.wait_until_fona_command_response:
+            print self.read_from_fona(5)
+        self.write_to_fona('AT+CMGS="' + cleaned_number + '"')
+        self.wait_until_fona_command_response()
+        print self.read_from_fona(5)
+        self.write_to_fona('\r')
+        self.wait_until_fona_command_response()
+        print self.read_from_fona(5)
+        print self.write_to_fona(text + '\x1a')
+        self.wait_until_fona_command_response()
+
+        self.command_history.append("self.read_from_fona()")
+        print self.read_from_fona(2)
         print "Check phone"
+
+
         return True
 
     def get_messages(self):
@@ -177,13 +366,15 @@ if __name__ == '__main__':
     allowed_numbers = {'2066795094', '18558655971'}
     serial_connection = serial.Serial('/dev/ttyUSB0', 9600)
     fona = Fona("Fona", serial_connection, allowed_numbers)
-    print fona.enable_verbose_errors()
-    print fona.get_carrier()
-    print fona.get_battery_state()
-    print fona.get_signal_strength()
-    print fona.get_module_name()
-    print fona.get_sim_card_number()
-    print fona.get_all_messages()
+    # fona.get_carrier()
+    battery_condition = fona.get_current_battery_condition()
+    fona.send_message("2066795094", "Time:" + str(time.time())
+                                    + ", PCT:" + str(battery_condition.battery_percent)
+                                    + ", mAH:" + str(battery_condition.milliamp_hours))
+    # fona.get_signal_strength()
+    # print fona.get_module_name()
+    # print fona.get_sim_card_number()
+    # print fona.get_all_messages()
 
     # for message in fona.get_messages():
     #    print "ID:" + message.message_id
@@ -193,5 +384,5 @@ if __name__ == '__main__':
     #    print "Stat:" + message.message_status
     #    print "SMS:" + message.message_text
 
-#   fona.get_messages(False)
-#   print fona.send_message('2066795094', 'test')
+    # fona.get_messages(False)
+    # print fona.send_message('2066795094', 'test')
